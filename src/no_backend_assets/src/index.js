8c43e4c62423bdc6aa79677728222d687a79db9a
MarsPool.XYZ <marspoolxyz@gmail.com>
2021-07-31 01:48:59 +0400

    Generate account id

--- src/no_backend_assets/src/index.js
+++ src/no_backend_assets/src/index.js
@@ -1,4 +1,8 @@
 import { Actor, HttpAgent } from '@dfinity/agent';
+import { sha224 } from '@dfinity/principal/lib/esm/utils/sha224';
+import { getCrc32 } from '@dfinity/principal/lib/esm/utils/getCrc';
+import { Principal } from "@dfinity/agent";  
+
 // Imports and re-exports candid interface
 import { idlFactory } from './ledger.did.js';
 export { idlFactory } from './ledger.did.js';
@@ -31,12 +35,62 @@ export const createActor = (canisterId, options) => {
  export const ledger = createActor(canisterId);
 
  document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  const greeting = await ledger.account_balance_dfx(name);
+  //console.log(Principal.fromText(name));
 
-  document.getElementById("greeting").innerText = greeting;
+  const name = document.getElementById("name").value.toString();
+  const index = document.getElementById("account-index").value.toString();
+
+  console.log(principalToAccountIdentifier(name,index));
+
+  document.getElementById("greeting").innerText = principalToAccountIdentifier(name,index);
+
+  var arg = {"account":principalToAccountIdentifier(name,index)}
+
+  console.log(ledger.account_balance_dfx(arg));
+
+  const greeting = await ledger.account_balance_dfx(arg);
+  console.log(greeting.e8s.value);
+  
+  
 });
 
+
+const to32bits = num => {
+  let b = new ArrayBuffer(4);
+  new DataView(b).setUint32(0, num);
+  return Array.from(new Uint8Array(b));
+}
+
+
+const principalToAccountIdentifier = (p, s) => {
+  const padding = Buffer("\x0Aaccount-id");
+  const array = new Uint8Array([
+      ...padding,
+      ...Principal.fromText(p).toBlob(),
+      ...getSubAccountArray(s)
+  ]);
+  const hash = sha224(array);
+  const checksum = to32bits(getCrc32(hash));
+  const array2 = new Uint8Array([
+      ...checksum,
+      ...hash
+  ]);
+  return toHexString(array2);
+};
+const toHexString = (byteArray)  =>{
+  return Array.from(byteArray, function(byte) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('')
+}
+
+const getSubAccountArray = (s) => {
+  if (Array.isArray(s)){
+    return s.concat(Array(32-s.length).fill(0));
+  } else {
+    //32 bit number only
+    return Array(28).fill(0).concat(to32bits(s ? s : 0))
+  }
+};
 /*
 import { idlFactory as no_backend_idl, canisterId as no_backend_id } from 'dfx-generated/no_backend';
 
